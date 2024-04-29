f3f239c15d65b716ab93d0b17d5a1b9c9611aa76
Vann Liu <vann@cpcp.tw>
2024-04-29 17:42:23 +0800

    update

--- src/components/custom/LinkWithLang.tsx
+++ src/components/custom/LinkWithLang.tsx
@@ -1,20 +1,5 @@
+"use client"
 import { i18n } from '~~/i18n.config'
-import LinkWithLang from 'vanns-common-modules/dist/components/next/LinkWithLang'
-
-interface TypeProps {
-  href?: string
-  lang?: string | string[]
-  children?: React.ReactNode
-  [key:string]: any
-}
-
-export default function LinkWithLangWrapper({href, lang, children, ...props}:TypeProps, ref:React.ReactNode){
-  if( !href ){
-    return <span {...props}>{children}</span>
-  }
-
-  const isDefaultLang = lang === i18n.defaultLocale.shortCode
-  const path = isDefaultLang ?href :`/${lang}${href}`
-
-  return <LinkWithLang {...props} href={path} lang={lang} defaultLang={i18n.defaultLocale.shortCode}>{children}</LinkWithLang>
-}
+import { createLinkWithLangComponent } from "vanns-common-modules/dist/components/next/CreateLinkWithLang"
+const LinkWithLang = createLinkWithLangComponent(i18n.defaultLocale.shortCode)
+export default LinkWithLang
