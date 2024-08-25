463e24ce4d7e072a9eba2622688efd7dbd32370e
Vann Liu <vann@cpcp.tw>
2024-08-25 14:30:04 +0800

    fix: layout

--- src/app/[lang]/about/horizon/(sections)/History.tsx
+++ src/app/[lang]/about/horizon/(sections)/History.tsx
@@ -17,7 +17,7 @@ function History(props:TypeProps, ref:React.ReactNode){
     <div className={twMerge('mb-16', className)}>
       <div className="container">
         <div className="mb-4 text-center text-gray-300"><T text="History"/></div>
-        <div className="mx-auto w-full max-w-[900px] text-gray-700">{props?.content}</div>
+        <pre className="mx-auto w-full max-w-[900px] text-gray-700" dangerouslySetInnerHTML={{__html: props?.content || ''}}></pre>
       </div>
     </div>
   </Suspense>
