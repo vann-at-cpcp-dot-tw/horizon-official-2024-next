e2a6db5ebda56a28713598c2336c18be3ed3867b
Vann Liu <vann@cpcp.tw>
2024-10-08 22:14:58 +0800

    fix: layout

--- src/app/[lang]/team/page.tsx
+++ src/app/[lang]/team/page.tsx
@@ -39,7 +39,7 @@ async function PageTeam({params}:TypeProps){
         <div className="row xl:row-gap-8 justify-center">
           {
             data?.teamPageSettings?.teamPageCustomFields?.teamMembers?.map((node:{[key:string]:any}, index:number)=>{
-              return <div className="lg:col-3 col-12 mb-8" key={index}>
+              return <div className="lg:col-3 col-6 mb-8" key={index}>
                 <RatioArea className="mb-3" ratio="114.28">
                   <Image className="absolute left-0 top-0 size-full" src={node?.image?.node?.mediaItemUrl || ''} fill={true} alt="" />
                 </RatioArea>
