3fd8d7bfe3530dd020149c5b636c90c0ba042b60
Vann Liu <vann@cpcp.tw>
2025-04-14 20:16:24 +0800

    fix: typo

--- src/app/[lang]/models/[seriesSlug]/(sections)/SingleSeriesTop.tsx
+++ src/app/[lang]/models/[seriesSlug]/(sections)/SingleSeriesTop.tsx
@@ -42,7 +42,7 @@ function SingleSeriesTop(props:TypeProps, ref:React.ReactNode){
       }
     ]} />
     <div className={twMerge('container max-w-[975px] lg:py-16 py-8', className)}>
-      <div className="serif mb-2 text-center text-[32px] text-golden-900 lg:text-[40px]">{seriesData?.name} aa { __('Series') }</div>
+      <div className="serif mb-2 text-center text-[32px] text-golden-900 lg:text-[40px]">{seriesData?.name} { __('Series') }</div>
       <div className="serif mb-3 text-center text-[24px] leading-[1.3] text-minor-900 lg:mb-6 lg:text-[40px]">{seriesData?.yachtsSeriesCustomFields?.seriesSimpleDescription}</div>
       <div className="text-center text-gray-700">{seriesData?.description}</div>
     </div>
